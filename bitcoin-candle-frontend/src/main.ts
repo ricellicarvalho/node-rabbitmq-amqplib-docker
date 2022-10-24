d57e32be77e0df0d688013ca475fa9ef7228e0b5
Ricelli Carvalho <ricelli.martinsdecarvalho@gmail.com>
2022-10-24 15:14:51 -0300

    Initializing the project

--- bitcoin-candle-frontend/src/main.ts
+++ bitcoin-candle-frontend/src/main.ts
@@ -1,5 +1,9 @@
 import { createApp } from 'vue'
+import VueApexCharts from 'vue3-apexcharts'
 import App from './App.vue'
 import store from './store'
 
-createApp(App).use(store).mount('#app')
+createApp(App)
+    .use(store)
+    .use(VueApexCharts)
+    .mount('#app')
--- bitcoin-candle-frontend/src/store/index.ts
+++ bitcoin-candle-frontend/src/store/index.ts
@@ -1,14 +1,8 @@
 import { createStore } from 'vuex'
+import CandleStore from './modules/CandleStore'
 
 export default createStore({
-  state: {
-  },
-  getters: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
   modules: {
+    CandleStore
   }
 })
